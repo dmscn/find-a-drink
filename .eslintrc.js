4723951e439c62032d07fb9713071e633cba65c0
Leo Damasceno <ldamasceno.dev@gmail.com>
2020-05-14 21:36:10 -0300

    feat: eslint setup

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+    env: {
+        browser: true,
+        es6: true,
+        node: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:prettier/recommended',
+        'prettier',
+        'prettier/react',
+    ],
+    plugins: ['react', 'prettier'],
+    parser: 'babel-eslint',
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+    },
+    settings: {
+        react: {
+            pragma: 'React',
+            version: 'detect',
+        },
+    },
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 2018,
+        sourceType: 'module',
+    },
+    rules: {
+        'prettier/prettier': 'error',
+        'react/prop-types': 'off',
+    },
+}
