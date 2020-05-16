ba71e4b810a30b86e87215190f39abe1f615a62f
Leo Damasceno <ldamasceno.dev@gmail.com>
2020-05-16 12:43:31 -0300

    style: global styles

---
+++ src/globalStyles.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  html, body, #app {
+    height: 100%;
+    width: 100%;
+    margin: 0;
+    padding: 0;
+  }
+`
+
+export default GlobalStyle
