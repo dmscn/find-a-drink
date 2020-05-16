657138bf5f35d7c5d0da3f55a098921769370c40
Leo Damasceno <ldamasceno.dev@gmail.com>
2020-05-16 12:51:03 -0300

    style: CDN importing fonts at globalStyles

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -1,6 +1,8 @@
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&family=Source+Sans+Pro:wght@400;600;700&display=swap');
+  
   html, body, #app {
     height: 100%;
     width: 100%;
