3dddd8a8e39a98650b29460a315b851f322415b1
Leo Damasceno <ldamasceno.dev@gmail.com>
2020-05-18 21:17:55 -0300

    fix: Root elements positions

--- src/App.js
+++ src/App.js
@@ -1,30 +1,34 @@
-import { ThemeProvider } from 'styled-components'
-import Home from '@pages/Home'
-import Products from '@pages/Products'
-import StoreProvider from '@store'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { ThemeProvider } from 'styled-components'
+import theme from '@assets/style/theme'
+import { Provider as StoreProvider } from 'react-redux'
+import store from '@store'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
 import GlobalStyled from './globalStyles'
 
-import theme from '@assets/style/theme'
+import Home from '@pages/Home'
+import Products from '@pages/Products'
 
 const App = () => (
-  <Router>
-    <GlobalStyled />
-    <ThemeProvider theme={theme}>
-      <StoreProvider>
-        <Switch>
-          <Route path="/products">
-            <Products />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
+  <ThemeProvider theme={theme}>
+    <React.Fragment>
+      <GlobalStyled />
+      <StoreProvider store={store}>
+        <Router history={history}>
+          <Switch>
+            <Route path="/products">
+              <Products />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </Router>
       </StoreProvider>
-    </ThemeProvider>
-  </Router>
+    </React.Fragment>
+  </ThemeProvider>
 )
 
 ReactDOM.render(<App />, document.getElementById('app'))
--- src/assets/style/theme.js
+++ src/assets/style/theme.js
@@ -3,8 +3,6 @@ import fonts from './fonts'
 import elevations from './elevations'
 import spacing from './spacing'
 
-console.log({ colors, fonts, elevations, spacing })
-
 export default {
   colors,
   fonts,
