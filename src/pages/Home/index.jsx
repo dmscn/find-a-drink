7044d8bb2e11283f6daea306fa99e7576a811e93
Leo Damasceno <ldamasceno.dev@gmail.com>
2020-05-22 12:37:25 -0300

    feat(Home Page): invoking useLocation() hook

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,38 @@
 import React from 'react'
-const Home = () => <div>Hello from Home page</div>
+import TextField from '@components/TextField'
+import useLocation from '@hooks/useLocation'
+
+const Home = () => {
+  const [location, setLocation] = React.useState('')
+  const [suggestions, setSuggestions] = React.useState([])
+  const { getLocation } = useLocation()
+
+  const getSuggestions = async query => {
+    try {
+      const locations = await getLocation(query)
+      setSuggestions(locations)
+    } catch (err) {
+      console.error(err)
+      setSuggestions([])
+    }
+  }
+
+  const handleTyping = event => {
+    const { value } = event.target
+    setLocation(value)
+    getSuggestions(value)
+  }
+
+  return (
+    <>
+      <TextField
+        placeholder="Digite sua localização"
+        value={location}
+        onChange={handleTyping}
+      />
+      {suggestions}
+    </>
+  )
+}
+
 export default Home
