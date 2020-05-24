36302a2bc1462e1c2bc2d9391f0babe833d12de4
Leo Damasceno <ldamasceno.dev@gmail.com>
2020-05-24 19:56:45 -0300

    fix(Components): change Page component extension to .jsx

--- src/components/Page/index.jsx
+++ src/components/Page/index.jsx
@@ -4,11 +4,13 @@ const Page = styled.div`
   ${({ theme: { colors } }) => css`
     display: flex;
     flex-direction: column;
+    flex: 1;
     width: 100%;
-    height: 100vh;
+    height: 100%;
     justify-content: flex-start;
     align-items: flex-start;
     background-color: ${colors.gray[1]};
+    overflow: auto;
   `}
 `
 export default Page
