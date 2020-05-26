4848e0237b3b374e6ec4e15c5d3b3a1e73344932
Leo Damasceno <ldamasceno.dev@gmail.com>
2020-05-26 20:12:27 -0300

    feat: jest + react-testing-library setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,38 @@
 module.exports = {
-    env: {
-        browser: true,
-        es6: true,
-        node: true,
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+    'prettier/react',
+  ],
+  plugins: ['react', 'prettier'],
+  parser: 'babel-eslint',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  settings: {
+    react: {
+      pragma: 'React',
+      version: 'detect',
     },
-    extends: [
-        'eslint:recommended',
-        'plugin:react/recommended',
-        'plugin:prettier/recommended',
-        'prettier',
-        'prettier/react',
-    ],
-    plugins: ['react', 'prettier'],
-    parser: 'babel-eslint',
-    globals: {
-        Atomics: 'readonly',
-        SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    settings: {
-        react: {
-            pragma: 'React',
-            version: 'detect',
-        },
-    },
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true,
-        },
-        ecmaVersion: 2018,
-        sourceType: 'module',
-    },
-    rules: {
-        'prettier/prettier': 'error',
-        'react/prop-types': 'off',
-    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'react/prop-types': 'off',
+  },
 }
---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  clearMocks: true,
+  testMatch: ['<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}'],
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+}
