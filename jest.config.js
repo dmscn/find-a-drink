eb3e83aad7041066c7648923d92e46094393c2c8
Leo Damasceno <ldamasceno.dev@gmail.com>
2020-05-28 11:10:48 -0300

    test: setup aliases at jest config

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,15 @@ module.exports = {
   clearMocks: true,
   testMatch: ['<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}'],
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+  moduleFileExtensions: ['js', 'jsx'],
+  moduleNameMapper: {
+    '^@components(.*)$': '<rootDir>src/components$1',
+    '^@pages(.*)$': '<rootDir>src/pages$1',
+    '^@utils(.*)$': '<rootDir>src/utils$1',
+    '^@helpers(.*)$': '<rootDir>src/helpers$1',
+    '^@hooks(.*)$': '<rootDir>/src/hooks$1',
+    '^@store(.*)$': '<rootDir>src/store$1',
+    '^@services(.*)$': '<rootDir>src/services$1',
+    '^@assets(.*)$': '<rootDir>src/assets$1',
+  },
 }
